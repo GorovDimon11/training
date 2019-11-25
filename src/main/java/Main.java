d30c84c7e86d67758a5ba4781e31bd242ba5c178
Dmitriy <dmitriygorovenko11@gmail.com>
2019-11-26 00:15:06 +0200

    Refactor repository

--- src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Model model = new Model();
-        View view = new View();
-        Controller controller = new Controller(view, model);
-        controller.processUser();
-    }
-}
