1ddec0783adc65b7cb290c60aae70f032a17c96d
DMYTRO <GorovDimon11>
2020-09-17 00:14:14 +0300

    Create Useless.java

---
+++ CashMaсhine_Spring/src/main/java/com/gmail/dmitriy/service/Useless.java
@@ -0,0 +1,14 @@
+﻿package com.gmail.dmitriy.service;
+
+import com.gmail.dmitriy.entity.Product;
+import com.gmail.dmitriy.exception.ProductExistException;
+import com.gmail.dmitriy.exception.ProductNotFoundException;
+import com.gmail.dmitriy.repository.ProductRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+public class Useless {
+
+}
