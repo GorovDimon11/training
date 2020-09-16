52553b1d33ceb19ff1c30b2292b092544d98e165
DMYTRO <GorovDimon11>
2020-09-16 23:53:33 +0300

    Update

--- CashMaсhine_Spring/src/main/java/com/gmail/dmitriy/exceptions/UserExistException.java
+++
@@ -1,8 +0,0 @@
-package com.gmail.dmitriy.exceptions;
-
-public class UserExistException extends Exception {
-    @Override
-    public String getMessage() {
-        return "User is already registered";
-    }
-}
